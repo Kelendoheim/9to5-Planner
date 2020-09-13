5af2976778598fb5747f37d90a718a01952ce6d5
TDWalsh <thomas.walsh736@gmail.com>
2020-09-12 23:36:46 -0400

    added comments into the js file and edited the readme to be informative

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # 9to5-Planner
+
+## Description
+
+This is a webpage that can be used as a daily planner. It will display the current date and use background colors within each row to indicate whether or not each hour has occured on that day. It can also store text into the local storage for each hour so the user can effectively display their plans for the day.
+
+## Usage
+
+This site can be used to check the date, the current hour in relationship to the 9 to 5 workday and it can store text into local storage to be displayed for each hour within the workday.
+
+## Visuals
+<img width="1343" alt="Screen Shot 2020-09-12 at 11 30 31 PM" src="https://user-images.githubusercontent.com/68260323/93009537-778adf00-f550-11ea-9a8f-0aa73eab7ec8.png">
+
+<img width="1324" alt="Screen Shot 2020-09-12 at 11 31 31 PM" src="https://user-images.githubusercontent.com/68260323/93009543-84a7ce00-f550-11ea-9ba9-f5b80470f188.png">
+
+<img width="1290" alt="Screen Shot 2020-09-12 at 11 32 06 PM" src="https://user-images.githubusercontent.com/68260323/93009546-8e313600-f550-11ea-8322-5a34870c4762.png">
+
+
+## Links 
+Link to the github deployed site:
+https://kelendoheim.github.io/9to5-Planner/
+Link to the github repo:
+https://github.com/Kelendoheim/9to5-Planner
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Authors and Acknowledgement
+I, Thomas Walsh, am the soul author of this project. I would like to acknowledge the Full Stack Flex coding bootcamp powered by Trilogy Education Services for all resources and instruction regarding this project.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//Below are the variables that use moment.js to store the current date and current hour
 var currentDayText = moment().format("MMMM Do YYYY");
 var currentHour = moment().hour();
+//created a variable to manipulate the container element 
 var containerEl = $(".container");
+//the following array will be used to set an id equal to the hour for that time block, which will be checked against the hour variable set from moment.js. This id will also be used as the key for local storage.
 var plannerHourId = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// the following array will be used to display the hour for each row in the planner.
 var workHours = [
   "9:00 AM",
   "10:00 AM",
@@ -13,15 +17,15 @@ var workHours = [
   "4:00 PM",
   "5:00 PM",
 ];
-
+//the line below will display the date in the current day in the header.
 $("#currentDay").text(currentDayText);
-
+// the following for loop will do most of the heavy lifting on this page. its utility is threefold: populate the container element, set the text if there is any in local storage and color each textarea based on the current hour.
 for (i = 0; i < workHours.length; i++) {
   var rowEl = $("<row>");
   var hourDisplay = $("<div>");
   var text = $("<textarea>");
   var buttons = $("<button>");
-
+// It will populate the page with the rows for each hour in the planner, inside of which will be the hour, text, and button sections. It will also give each row a corresponding id from the plannerHourId array.
   rowEl.attr("class", "time-block row");
   hourDisplay.attr("class", "hour col-sm-2");
   text.attr("class", "description col-sm-8");
@@ -29,8 +33,9 @@ for (i = 0; i < workHours.length; i++) {
   rowEl.attr("id", plannerHourId[i]);
   hourDisplay.text(workHours[i]);
   buttons.text("Save");
+  // the next line of code will populate the text area with text if any has been set to local storage
   text.text(localStorage.getItem(plannerHourId[i]));
-
+// the following for loop will set the textarea with a class to designate whether the hour of that row is before during or after the current hour.
   if (plannerHourId[i] > currentHour) {
     text.attr("class", "future description col-sm-8");
   } else if (plannerHourId[i] < currentHour) {
@@ -44,10 +49,10 @@ for (i = 0; i < workHours.length; i++) {
   rowEl.append(buttons);
   containerEl.append(rowEl);
 }
-
+// the following code will be the event listener for clicking the save button. it will then set into local storage the id of the row as the key and the text of the textarea as the value
 $(document).on("click", ".saveBtn", function (event) {
-  console.log($(event.target).parent().attr("id"));
-  console.log($(event.target).siblings(".description").val());
+  console.log($(event.target).parent().attr("id")); //key
+  console.log($(event.target).siblings(".description").val()); //value
   localStorage.setItem(
     $(event.target).parent().attr("id"),
     $(event.target).siblings(".description").val()
