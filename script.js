9d75cd2484f2ce7171e261174686d20237c0a69f
TDWalsh <thomas.walsh736@gmail.com>
2020-09-12 22:27:11 -0400

    added an event listener that correctly listens for which save button is pressed and stores data from the sibling textarea into local storage

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var currentDayText = moment().format("MMMM Do YYYY");
 var currentHour = moment().hour();
 var containerEl = $(".container");
 
-
 var plannerHourId = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var workHours = [
   "9:00 AM",
@@ -13,39 +12,44 @@ var workHours = [
   "2:00 PM",
   "3:00 PM",
   "4:00 PM",
-  "5:00 PM"
+  "5:00 PM",
 ];
 $("#currentDay").text(currentDayText);
-for(i = 0; i < workHours.length; i++){
-    var rowEl = $("<row>");
-    var hourDisplay = $("<div>");
-    var text = $("<textarea>");
-    var buttons = $("<button>");
+for (i = 0; i < workHours.length; i++) {
+  var rowEl = $("<row>");
+  var hourDisplay = $("<div>");
+  var text = $("<textarea>");
+  var buttons = $("<button>");
 
-    rowEl.attr("class", "time-block row");
-    hourDisplay.attr("class", "hour col-sm-2");
-    text.attr("class", "description col-sm-8");
-    buttons.attr("class", "saveBtn col-sm-2");
-    rowEl.attr("id", plannerHourId[i])
-    hourDisplay.text(workHours[i]);
-    buttons.text("Save");
+  rowEl.attr("class", "time-block row");
+  hourDisplay.attr("class", "hour col-sm-2");
+  text.attr("class", "description col-sm-8");
+  buttons.attr("class", "saveBtn col-sm-2");
+  rowEl.attr("id", plannerHourId[i]);
+  hourDisplay.text(workHours[i]);
+  buttons.text("Save");
 
-    if (plannerHourId[i] > currentHour){
-        text.attr("class", "future description col-sm-8");
-    } else if (plannerHourId[i] < currentHour){
-        text.attr("class", "past description col-sm-8");
-    } else {
-        text.attr("class", "present description col-sm-8")
-    }
-    console.log(plannerHourId[i]);
-    console.log(currentHour);
+  if (plannerHourId[i] > currentHour) {
+    text.attr("class", "future description col-sm-8");
+  } else if (plannerHourId[i] < currentHour) {
+    text.attr("class", "past description col-sm-8");
+  } else {
+    text.attr("class", "present description col-sm-8");
+  }
 
-    rowEl.append(hourDisplay);
-    rowEl.append(text);
-    rowEl.append(buttons);
-    containerEl.append(rowEl);
+  rowEl.append(hourDisplay);
+  rowEl.append(text);
+  rowEl.append(buttons);
+  containerEl.append(rowEl);
+}
 
-    
+function populateText() {}
 
-};
-
+$(document).on("click", ".saveBtn", function (event) {
+  console.log($(event.target).parent().attr("id"));
+  console.log($(event.target).siblings(".description").val());
+  localStorage.setItem(
+    $(event.target).parent().attr("id"),
+    $(event.target).siblings(".description").val()
+  );
+});
